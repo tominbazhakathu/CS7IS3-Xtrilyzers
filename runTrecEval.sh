32ef5312cd313a58a3fc976b99bf7a2a5bcc7094
Tomin B Azhakathu <tominbazhakathu@gmail.com>
2018-11-28 13:24:37 +0000

    Changed Run Scripts

--- runTrecEval.sh
+++ runTrecEval.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "Runing Trec Eval with $1"
-trec_eval.9.0/trec_eval qrels/$1 results/results.txt > "evaluation/trec_eval_result"
-echo "Output generated at evaluation/trec_eval_result"
+trec_eval.9.0/trec_eval qrels/$1 results/results.txt
+#echo "Output generated at evaluation/trec_eval_result"
